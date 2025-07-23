15a2f6b36057b0a02322a8225ac258fd446e4020
kien-dev <kienpt313@gmail.com>
2025-07-23 14:56:54 +0700

    update api find all usser

--- src/main/java/teamit/hust/ktxcdshustbe/repository/studentRegisterRoom/impl/StudentRegisterRoomRepositoryImpl.java
+++ src/main/java/teamit/hust/ktxcdshustbe/repository/studentRegisterRoom/impl/StudentRegisterRoomRepositoryImpl.java
@@ -344,8 +344,8 @@ public class StudentRegisterRoomRepositoryImpl implements StudentRegisterRoomRep
                 dto.setCodeSemester(ValueUtil.getStringByObject(obj[7]));
                 dto.setTitleSemester(ValueUtil.getStringByObject(obj[8]));
                 dto.setIdTimeHired(ValueUtil.getIntegerByObject(obj[9]));
-                dto.setTimeHiredStarted(ValueUtil.getLongByObject(obj[10]));
-                dto.setTimeHiredEnded(ValueUtil.getLongByObject(obj[11]));
+                dto.setTimeHiredStarted(ValueUtil.getStringByObject(obj[10]));
+                dto.setTimeHiredEnded(ValueUtil.getStringByObject(obj[11]));
                 userRegisterRoomDtos.add(dto);
             }
         }
