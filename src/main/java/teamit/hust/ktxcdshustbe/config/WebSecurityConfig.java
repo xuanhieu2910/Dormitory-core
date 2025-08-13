53827c82b1a3b4364d5078d1708d6f3d89d2193c
hieuvx <hieuxuanvuong29102000@gmail.com>
2025-08-13 10:18:18 +0700

    update is initialize profile

--- src/main/java/teamit/hust/ktxcdshustbe/config/WebSecurityConfig.java
+++ src/main/java/teamit/hust/ktxcdshustbe/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     InterceptLog logInterceptor;
     public final static String INFORMATION_USER = "informationUser";
     public final static String DOMAIN_FE = "https://ktx-staging.hust.edu.vn";
-    public final static String DOMAIN_FPT_PAYMENT = "https://portal-v2-staging.foxpay.vn";
+    public final static String DOMAIN_FPT_PAYMENT = "https://portal-v2.foxpay.vn";
     public final static String DOMAIN_BE = "https://ktx-staging-be.hust.edu.vn/ktx-be";
 
     @Bean
