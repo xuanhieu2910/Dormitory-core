9766b59c907bd626733ee0d5dc573d9e2d8867d2
hieuvx <hieuxuanvuong29102000@gmail.com>
2025-08-13 14:42:25 +0700

    update is initialize profile

--- src/main/java/teamit/hust/ktxcdshustbe/service/studentRegisterRoom/impl/StudentRegisterRoomServiceImpl.java
+++ src/main/java/teamit/hust/ktxcdshustbe/service/studentRegisterRoom/impl/StudentRegisterRoomServiceImpl.java
@@ -340,6 +340,9 @@ public class StudentRegisterRoomServiceImpl implements StudentRegisterRoomServic
         if (null == request.getCodeUserRegister() || null == request.getStatus()) {
             throw new ValidParametersException();
         }
+        if (null == request.getCodeUserRegister() || null == request.getStatus()) {
+            throw new ValidParametersException();
+        }
         if (!request.getStatus().equals(Constants.STUDENT_REGISTER_ROOM_STATUS_ACCEPT) &&
                 !request.getStatus().equals(Constants.STUDENT_REGISTER_ROOM_STATUS_NOT_ACCEPT)){
             throw new ValidParametersException();
